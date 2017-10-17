5d2ff8ea8af2088e3aec4861fbdb386f66333798
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-10-17 23:41:42 +0800

    added container for section 1

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,12 @@
 
 
 @section('content')
-
+    <div class="section1">
+        <div class="row">
+            
+        </div>
+    
+    </div>
     <h1>Hello World</h1>
     <button class="btn btn-primary" onclick='horay()'>Click Me</button>
 
