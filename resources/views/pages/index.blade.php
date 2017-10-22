093d8e075ea3150c8f4e2d354d06f952c3441b25
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-10-23 01:16:42 +0800

    added all 6 slides content

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -110,6 +110,39 @@
                                     FOUNDED
                                 </span>
                             </div>
+                            <div class="slide">
+                                <p>The founder consulted experts and professionals in healthcare and education</p>
+                                <span class="content-container">
+                                    RECRUITING CONSULTANTS
+                                </span>
+                            </div>
+                            <div class="slide">
+                                <p>More youth and professionals are integrated in the community</p>
+                                <span class="content-container">
+                                    RECRUITING MEMBERS
+                                </span>
+                            </div>
+                            <div class="slide">
+                                <p>'Wonder Woman' forum, focusing on sexual education for female teenagers</p>
+                                <span class="content-container">
+                                    FIRST PROJECT
+                                </span>
+                            </div>
+
+                            <div class="slide">
+                                <p>First overseas project: a healthcare seminar in Rumah Pemulihan Kasih Anugerah orphanage in Jakarta, Indonesia</p>
+                                <span class="content-container">
+                                    SECOND PROJECT
+                                </span>
+                            </div>
+
+                            <div class="slide">
+                                <p>Literary seminar, teaching under privileged children in Jakarta, Indonesia, how to read and write</p>
+                                <span class="content-container">
+                                    THIRD PROJECT
+                                </span>
+                            </div>
+                            
                             <a href="#" class="prev glyphicon glyphicon-arrow-left"></a>
                             <a href="#" class="next glyphicon glyphicon-arrow-right"></a>
                         </div>
