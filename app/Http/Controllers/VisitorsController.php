d336d11e70f1cae2998381bd7f5645defe98e7e0
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-10-29 09:52:35 +0800

    configure the first email to the mailtrap

--- app/Http/Controllers/VisitorsController.php
+++ app/Http/Controllers/VisitorsController.php
@@ -4,19 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Visitor;
+use App\Mail\VisitorContact;
 
 class VisitorsController extends Controller
 {
     public function send() {
-        // create a new visitor from the requested data and save it to the database
-        $visitor = new Visitor;
-        
-        Visitor::create([
+        // create a new visitor from the requested data
+        $visitor = [
             'name' => request('name'),
             'email' => request('email'),
             'message' => request('message')
-        ]);
+        ];
+        // Send the email to the administrator
+        \Mail::to('admin@infinitudecommunity.org')->send(new VisitorContact($visitor));
 
+        if (! \Mail::failures()) {
+            // upon sucessful, save it to the database
+            Visitor::create($visitor);
+        }
+        // Redirect to the bottom page
         return redirect('/#contact-us');
     }
 }
