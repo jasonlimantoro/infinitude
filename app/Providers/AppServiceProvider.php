3c9dec674b901bc42d1e995c0c47c0fdeb243da2
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-10-31 03:08:42 +0800

    set a default stringLength to prevent database error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,8 +13,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
+    
     {
-        //
+        // set a default string length
+        Schema::defaultStringLength(191);
     }
 
     /**
