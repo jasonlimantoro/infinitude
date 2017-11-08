a7b029d58d50edced17b163e7d259e66feeafb85
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-11-09 02:12:31 +0800

    added be-involved-content-container

--- resources/views/layouts/sections/section6.blade.php
+++ resources/views/layouts/sections/section6.blade.php
@@ -25,7 +25,80 @@
                         </div>
                     
                     </div>
-                    <div class="col-md-8 be-involved-content">
+                    <div class="col-md-offset-1 col-md-6 be-involved-content-container">
+                        <div class="volunteer">
+                            <h2>BE OUR INFINITUDE BUDDY AND <br> START TO <span>MAKE A DIFFERENCE</span> TODAY</h2>
+                            <p>
+                                Are you looking for an opportunity to make a difference and contribute back to the community? INFINITUDE is looking for volunteers (INFINITUDE buddies) who have the passion in educating young adults and children. We have various opportunities you could volunteer for:
+                            </p>
+                            <ul>
+                                <li>Become a seminar facilitator</li>
+                                <li>Design creativity materials / booklet</li>
+                                <li>Set up booths at outreach and fundraising events</li>
+                                <li>Take various ad-hoc projects</li>
+                            </ul>
+
+                            <p>
+                                <img src="/images/icons/form.png" alt="form" class="icon">
+                                Download the form <strong> <a href="#">here</a></strong> <br>
+                                <img src="/images/icons/email.png" alt="email" class="icon">
+                                Send the form to our <strong> <a href="#">email</a></strong>
+                            </p>
+                            {{--  <img src="/images/Be involved_Volunteer_BG.png" alt="involved" class="img-responsive background-content">     --}}
+                        </div>
+
+                        <div class="donator">
+                            <h2>EVERY GIVING COUNTS AND CONTRIBUTE TO <span>MAKE AN EVERLASTING </span> IMPACT TO OTHERS</h2>
+
+                            <p>
+                                INFINITUDE is a social community (non-profit) that provides healthcare education for young adults and children. As healthcare and education are key pillars in building our society in South East Asia, we believe that we can make an everlasting impact to the future generation to be one step healthier, smarter, and more prosperous. Support our mission of educating young adults and children to by making a donation today. <br>
+                                Thank you very much for your support.
+                            </p>
+
+                            <p>
+                                <img src="/images/icons/Donate.png" alt="donate">
+                                To make a donation click <a href="#"><strong>here</strong></a>
+                            </p>
+                            {{--  <img src="/images/Be involved_Donatur_BG.png" alt="donatur">  --}}
+
+                        </div>
+
+                        <div class="merchandise">
+                            <h2> SUPPORT US BY PURCHASING OUR <span>MERCHANDISE</span></h2>
+
+                            <p>
+                                Every purchase will fund our healthcare education programmes which are organized to enrich lives of young adults and under-privileged children in South East Asia. 
+                            </p>
+
+                            <div class="merchandise-images-container">
+                                <div class="row">
+                                    <div class="col-md-4 item">
+                                        <img src="/images/Be involved_merchandise_tshirt.png" alt="tshirt" class="img-responsive">
+                                        <p>INFINITUDE Tshirt   <strong class="price">$10</strong> </p>
+                                    </div>
+
+                                    <div class="col-md-4 item">
+                                        <img src="/images/Be involved_merchandise_umbrella.png" alt="umbrella" class="img-responsive">
+                                        <p>INFINITUDE Umbrella <strong class="price">$12</strong> </p>
+                                    </div>
+
+                                    <div class="col-md-4 item">
+                                        <img src="/images/Be involved_merchandise_mug.png" alt="mug" class="img-responsive">
+                                        <p>INFINITUDE Mug <strong class="price">$15</strong> </p>
+                                    </div>
+
+                                </div>
+                            
+                            </div>
+
+                            <p>
+                                <img src="/images/icons/email.png" alt="email" class="icon">
+                                To make a purchase, please <strong><a href="#">email</a></strong> us 
+                            
+                            </p>
+                        
+                        </div>
+
                     </div>
                 </div>
             </div>
