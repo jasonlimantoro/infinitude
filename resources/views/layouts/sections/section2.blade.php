6ace444d7737edc8b2ced51f6ca95d0b583883d6
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-07 03:35:26 +0800

    section2 is now mobile responsive

--- resources/views/layouts/sections/section2.blade.php
+++ resources/views/layouts/sections/section2.blade.php
@@ -7,40 +7,45 @@
         <div class="col-md-7 col-xs-12 img-about">
             <img src="/images/about_us_BG.png" alt="about us" class="img-responsive">
         </div>
-        <div class="col-md-4 visible-md title" id="vision-and-mission">
-            <div class="vision">
-                <img src="/images/icons/vision.png" alt="logo" class="img-responsive">
-                <h3>VISION</h3>
-                <p>
-                    To be a community that gives an everlasting impact in improving the quality of healthcare and education among youth and children in need in South East Asia region
-                </p>
-            </div>
-
-            <div class="mission">
-                <img src="/images/icons/mission.png" alt="logo" class="img-responsive">
-                <h3>MISSIONS</h3>
-                <div class="row-divider-mission clearfix">
-                    <div class="col-md-4">
+        <div class="col-md-4 col-xs-12 title" id="vision-and-mission">
+            <div class="row">
+                <div class="col-md-12 col-xs-12">
+                    <div class="vision">
+                        <img src="/images/icons/vision.png" alt="logo" class="img-responsive">
+                        <h3>VISION</h3>
                         <p>
-                            To promote health awareness and healthy lifestyle
+                            To be a community that gives an everlasting impact in improving the quality of healthcare and education among youth and children in need in South East Asia region
                         </p>
-                    </div>
-                    <div class="col-md-4">
-                        <p>
-                            To make healthcare educational materials fun and engaging
-                        </p>
-                        <div class="vl-1"></div> 
-                    </div>
-                    
-                    <div class="col-md-4">
-                    <p>
-                        To distribute basic healthcare and education needs to rural areas
-                    </p>
-                        <div class="vl-2"></div> 
                     </div>
                 </div>
             </div>
-        
+            <div class="row">
+                <div class="col-md-12 col-xs-12">
+                    <div class="mission">
+                        <img src="/images/icons/mission.png" alt="logo" class="img-responsive">
+                        <h3>MISSIONS</h3>
+                        <div class="row divider-mission clearfix">
+                            <div class="col-md-4 col-xs-4">
+                                <p>
+                                    To promote health awareness and healthy lifestyle
+                                </p>
+                            </div>
+                            <div class="col-md-4 col-xs-4">
+                                <p>
+                                    To make healthcare educational materials fun and engaging
+                                </p>
+                            </div>
+                            
+                            <div class="col-md-4 col-xs-4">
+                            <p>
+                                To distribute basic healthcare and education needs to rural areas
+                            </p>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
