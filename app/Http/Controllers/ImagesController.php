0061a4d8136cd5014f94673ca48fc233d552f513
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-12-09 15:31:34 +0800

    Intervention image

---
+++ app/Http/Controllers/ImagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImagesController extends Controller
+{
+    public function __construct($imgPath)
+    {
+        $this->imgPath = $imgPath;
+    }
+
+    public function intervent() {
+        $img = \Image::make(url($this->imgPath))->encode('data-url');
+        return $img;
+    }
+    
+}
--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Visitor;
+use App\Http\Controllers\ImagesController;
+
 class PagesController extends Controller
 {
     public function index() {
+        $img = new ImagesController('/images/Home_BG.png');
+        $bgImage = $img->intervent();
         $mailto = 'jasonlimantoro99@gmail.com';
-        return view('pages.index', compact('mailto'));
+        return view('pages.index', compact(['mailto', 'bgImage']));
     }
 
 }
